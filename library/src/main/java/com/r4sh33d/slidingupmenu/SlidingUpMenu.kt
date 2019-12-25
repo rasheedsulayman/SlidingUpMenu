619d60d64e4912d8e187317c983f1a73761284fc
Rasheed Sulayman <sulaymanrasheed@gmail.com>
2019-12-25 20:11:06 +0100

    Code formatting throughout the project.

--- library/src/main/java/com/r4sh33d/slidingupmenu/SlidingUpMenu.kt
+++ library/src/main/java/com/r4sh33d/slidingupmenu/SlidingUpMenu.kt
@@ -1,3 +1,18 @@
+/**
+ * Copyright (c) Rasheed Sulayman.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.r4sh33d.slidingupmenu
 
 import android.content.Context
@@ -37,7 +52,7 @@ class SlidingUpMenu(
     private val menuModelItems: List<MenuModel>? = null
 ) : BottomSheetDialog(context, getDialogTheme(context)) {
 
-    //Views
+    // Views
     internal val dialogRootView = context.inflate<LinearLayout>(R.layout.dialog_root_view)
     private val titleTextView = dialogRootView.findViewById<TextView>(R.id.dialogTitleTextView)
     private val viewPagerContainerLinearLayout =
@@ -47,7 +62,7 @@ class SlidingUpMenu(
 
     private val menuItemsList = mutableListOf<MenuModel>()
 
-    //Other fields
+    // Other fields
     internal var menuType = MenuType.GRID
     internal var scrollDirection = ScrollDirection.HORIZONTAL
     internal var menuModelSelectedListener: MenuModelSelectedListener? = null
@@ -82,7 +97,7 @@ class SlidingUpMenu(
     }
 
     private fun populateMenuModelItems() {
-        //Try to build the menu list
+        // Try to build the menu list
         if (menuResource != null) menuItemsList.addAll(context.getMenuList(menuResource))
         if (menuModelItems != null) menuItemsList.addAll(menuModelItems)
         require(menuItemsList.size > 0) {
@@ -106,7 +121,8 @@ class SlidingUpMenu(
     }
 
     fun titleText(
-        @StringRes titleRes: Int? = null, titleText: String? = null
+        @StringRes titleRes: Int? = null,
+        titleText: String? = null
     ): SlidingUpMenu {
         assertOneSet("title", titleText, titleRes)
         populateText(
--- sample/src/main/java/com/r4sh33d/slidingupmenu/SingleTestActivity.kt
+++ sample/src/main/java/com/r4sh33d/slidingupmenu/SingleTestActivity.kt
@@ -1,3 +1,18 @@
+/**
+ * Copyright (c) Rasheed Sulayman.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.r4sh33d.slidingupmenu
 
 import androidx.appcompat.app.AppCompatActivity
@@ -6,8 +21,6 @@ import androidx.annotation.DrawableRes
 import androidx.core.content.ContextCompat
 import com.r4sh33d.R
 import com.r4sh33d.slidingupmenu.utils.MenuModel
-import com.r4sh33d.slidingupmenu.utils.MenuType
-import com.r4sh33d.slidingupmenu.utils.ScrollDirection
 import kotlinx.android.synthetic.main.activity_single_test.*
 
 class SingleTestActivity : AppCompatActivity() {
@@ -16,7 +29,7 @@ class SingleTestActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_single_test)
 
-        val  menuItems = listOf(
+        val menuItems = listOf(
             MenuModel(1, "Model 1", getDrawableAsset(R.drawable.avast)),
             MenuModel(2, "Model 2", getDrawableAsset(R.drawable.zune)),
             MenuModel(3, "Model 3", getDrawableAsset(R.drawable.zune)),
